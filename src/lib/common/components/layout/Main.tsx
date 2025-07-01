fe3d81dbc99b40526141dd0747ab898fcd2a094c
José Daniel Grayson <josedanielgrayson@proton.me>
2025-07-01 14:03:26 -0500

    refactor: reduce main content padding a bit on smaller devices

--- src/lib/common/components/layout/Main.tsx
+++ src/lib/common/components/layout/Main.tsx
@@ -10,7 +10,7 @@ export const Main = ({ className, children, ...props }: Props) => (
     role="main"
     autoFocus
     className={twMerge(
-      'flex grow flex-col overflow-y-scroll px-8 py-6 focus:outline-none',
+      'flex grow flex-col overflow-y-scroll px-3 py-6 focus:outline-none sm:px-8',
       className,
     )}
   >
