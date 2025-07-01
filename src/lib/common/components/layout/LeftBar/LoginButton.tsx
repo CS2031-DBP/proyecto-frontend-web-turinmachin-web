ffa574071c0aa107267f01bcc0664a7a7e4319d4
dfigueroaw <dfigueroaw@gmail.com>
2025-06-30 22:34:08 -0500

    feat: improve desing and responsiveness of login button

--- src/lib/common/components/layout/LeftBar/LoginButton.tsx
+++ src/lib/common/components/layout/LeftBar/LoginButton.tsx
@@ -2,6 +2,7 @@
 
 import { usePopup } from '@/lib/common/hooks/use-popup';
 import { ButtonHTMLAttributes } from 'react';
+import { LuLogIn } from 'react-icons/lu';
 import { twMerge } from 'tailwind-merge';
 import { Button } from '../../Button';
 
@@ -15,9 +16,13 @@ export const LoginButton = ({ className, ...props }: Props) => {
       {...props}
       onClick={() => openPopup('login')}
       variant="special"
-      className={twMerge('w-full py-3 text-xl', className)}
+      className={twMerge(
+        'flex items-center justify-center py-3 text-xl not-sm:px-0',
+        className,
+      )}
     >
-      Iniciar sesión
+      <LuLogIn size={24} />
+      <span className="ml-2 not-sm:hidden">Iniciar sesión</span>
     </Button>
   );
 };
---
+++ src/lib/post/components/ToxicityPostPopup.tsx
@@ -0,0 +1,29 @@
+import { LuMailWarning, LuTriangleAlert } from 'react-icons/lu';
+
+export interface Props {
+  onClose: () => void;
+}
+
+export const VerificationPopup = ({ onClose }: Props) => (
+  <div className="bg-surface border-background-alt relative w-full max-w-sm rounded-xl border px-6 py-8">
+    <div className="mb-2 flex justify-center text-yellow-300">
+      <LuTriangleAlert className="h-10 w-10" />
+    </div>
+
+    <h2 className="mb-2 text-center text-xl font-bold">Contenido no seguro</h2>
+    <div>
+      <LuMailWarning size={40} />
+    </div>
+    <p>
+      Hemos detectado que tu publicación podría contener contenido no seguro.
+      Por favor, revisa y ajusta el mensaje antes de intentarlo nuevamente.
+      ¡Gracias por ayudarnos a mantener un espacio seguro para todos!
+    </p>
+    <button
+      onClick={onClose}
+      className="bg-special hover:bg-special-muted mb-3 w-full rounded py-2 font-bold text-white transition-colors ease-in-out"
+    >
+      Continuar
+    </button>
+  </div>
+);
