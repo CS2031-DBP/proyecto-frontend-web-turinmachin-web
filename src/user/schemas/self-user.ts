c1edd0c0cb09b1f19a2e42b94b206b5f1fe42045
José Daniel Grayson <josedanielgrayson@proton.me>
2025-07-12 15:18:23 -0500

    feat: add streakSafe field to SelfuserSchema

--- src/user/schemas/self-user.ts
+++ src/user/schemas/self-user.ts
@@ -4,6 +4,7 @@ import { UserSchema } from './user';
 export const SelfUserSchema = UserSchema.extend({
   authProvider: z.enum(['CREDENTIALS', 'GOOGLE']),
   hasPassword: z.boolean(),
+  streakSafe: z.boolean(),
 });
 
 export type SelfUserSchema = z.infer<typeof SelfUserSchema>;
