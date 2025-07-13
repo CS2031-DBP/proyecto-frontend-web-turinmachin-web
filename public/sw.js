fde53af3ef2939da3384337cc8b2569c3769d9ee
José Daniel Grayson <josedanielgrayson@proton.me>
2025-07-13 02:09:46 -0500

    fix: only show notifications when app is not focused

--- public/sw.js
+++ public/sw.js
@@ -1,11 +1,19 @@
-self.addEventListener('push', (event) => {
+self.addEventListener('push', async (event) => {
   if (!event.data) return;
 
   const data = event.data.json();
   if (!data.title) return;
 
-  self.registration.showNotification(data.title, {
-    vibrate: [100, 50, 100],
-    ...data,
+  const allClients = await self.clients.matchAll({
+    type: 'window',
+    includeUncontrolled: true,
   });
+
+  const isFocused = allClients.some((client) => client.focused);
+  if (!isFocused) {
+    self.registration.showNotification(data.title, {
+      vibrate: [100, 50, 100],
+      ...data,
+    });
+  }
 });
