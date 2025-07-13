6c6d03d97d0a689a975cbf5c18d29f05fda335b8
José Daniel Grayson <josedanielgrayson@proton.me>
2025-07-13 12:53:51 -0500

    refactor(comment): use modern recursive schema definition

--- src/comment/schemas/comment.ts
+++ src/comment/schemas/comment.ts
@@ -1,20 +1,15 @@
 import { z } from 'zod/v4';
 import { UserSchema } from '../../user/schemas/user';
 
-const BaseCommentSchema = z.object({
+export const CommentSchema = z.object({
   id: z.string(),
   author: UserSchema,
   content: z.string(),
+  get replies() {
+    return z.array(CommentSchema);
+  },
   createdAt: z.coerce.date(),
   updatedAt: z.coerce.date(),
 });
 
-export type CommentSchema = z.infer<typeof BaseCommentSchema> & {
-  replies: CommentSchema[];
-};
-
-export const CommentSchema: z.ZodType<CommentSchema> = BaseCommentSchema.extend(
-  {
-    replies: z.lazy(() => CommentSchema.array()),
-  },
-);
+export type CommentSchema = z.infer<typeof CommentSchema>;
