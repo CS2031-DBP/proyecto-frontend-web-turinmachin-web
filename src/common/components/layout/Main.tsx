3f86739ac21bb4fbc7073dcb8b9c4e18a36d5fc8
José Daniel Grayson <josedanielgrayson@proton.me>
2025-07-13 22:07:23 -0500

    fix: set Main z-index to 0

--- src/common/components/layout/Main.tsx
+++ src/common/components/layout/Main.tsx
@@ -10,7 +10,7 @@ export const Main = ({ className, children, ...props }: Props) => (
     role="main"
     autoFocus
     className={twMerge(
-      'flex grow flex-col overflow-y-auto px-3 py-6 focus:outline-none sm:px-8',
+      'z-0 flex grow flex-col overflow-y-auto px-3 py-6 focus:outline-none sm:px-8',
       className,
     )}
   >
